ae548649f88c2af09bd0baf8cb99eb10d3377f76
fakornam <benmorladza16@gmail.com>
2025-08-17 10:43:14 +0000

    hoping it works

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS Books (
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES Authors (author_id) 
+    FOREIGN KEY (author_id) REFERENCES Authors (author_id) 
         ON DELETE SET NULL ON UPDATE CASCADE
 );
 
@@ -24,21 +24,21 @@ CREATE TABLE IF NOT EXISTS Customers (
     address TEXT
 );
 
-CREATE TABLE IF NOT EXISTS Orders (
+CREATE TABLE IF NOT EXISTS Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id) 
+    customer_id INT NOT NULL ,
+    order_date DATE ,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) 
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
-    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
     quantity DOUBLE NOT NULL CHECK (quantity > 0),
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id) 
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),  
     ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES Books (book_id) 
+    FOREIGN KEY (book_id) REFERENCES Books(book_id) 
         ON DELETE CASCADE ON UPDATE CASCADE
 );
